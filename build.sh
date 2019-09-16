a8cc4abb20b2dc31abd71e98e4044f66e28110e4
Anton Indrawan <anton.indrawan@gmail.com>
2019-09-16 22:18:44 +0200

    Add travis CI

--- build.sh
+++ build.sh
@@ -9,7 +9,10 @@ readonly SOURCE_DIR=${SCRIPT_DIR}/src
 readonly OUTPUT_DIR=${SCRIPT_DIR}/output
 
 mkdir -p ${OUTPUT_DIR}
+cd ${OUTPUT_DIR}
 
-cmake -G Ninja -B ${OUTPUT_DIR} -S ${SOURCE_DIR} -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/install
+# 'cmake -S <source-dir> -B <build-dir>' is not yet supported in cmake used in the standard Ubuntu 18.04 (v3.10) or on travis' bionic (v3.12).
+# That has been supported since cmake 3.13. 
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/install ${SOURCE_DIR}
 cmake --build ${OUTPUT_DIR}
 cmake --build ${OUTPUT_DIR} --target install
